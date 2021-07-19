1b23d8854be7e70004ae34ac5dea242561b7b385
maximsmolyakov <sm.maksim@icloud.com>
2021-07-19 23:26:35 +0800

    Add AlignmentDeserializer.kt

---
+++ features/page/src/main/java/com/maximapps/page/data/deserializers/AlignmentDeserializer.kt
@@ -0,0 +1,50 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Maxim Smolyakov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.maximapps.page.data.deserializers
+
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.maximapps.page.data.models.Alignment
+import com.maximapps.page.exceptions.InvalidAlignmentTypeException
+import java.lang.reflect.Type
+
+/**
+ * Implementation of the JsonDeserializer interface for converting
+ * a jsonElement to the Alignment model.
+ *
+ * @since 0.1
+ */
+class AlignmentDeserializer : JsonDeserializer<Alignment> {
+    override fun deserialize(
+        json: JsonElement, typeOfT: Type, context: JsonDeserializationContext
+    ): Alignment =
+        when (json.asString) {
+            "start" -> Alignment.Start
+            "center" -> Alignment.Center
+            "end" -> Alignment.End
+            else -> throw InvalidAlignmentTypeException("Unknown alignment type")
+        }
+}
