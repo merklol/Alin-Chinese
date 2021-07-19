d24edcbaa7b557150b04acd744368de25d06659e
maximsmolyakov <sm.maksim@icloud.com>
2021-07-19 17:17:24 +0800

    Initial commit

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    mavenCentral()
+    google()
+}
+
+dependencies {
+    implementation("com.android.tools.build:gradle:7.1.0-alpha03")
+    implementation(kotlin("gradle-plugin", version = "1.5.20"))
+}
