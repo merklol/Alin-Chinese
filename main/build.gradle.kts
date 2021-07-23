a999c5e75717b7ff7ffd943da26739a7724980c4
maximsmolyakov <sm.maksim@icloud.com>
2021-07-23 18:59:00 +0800

    Migrate main module gradle files to Kotlin DSL

---
+++ main/build.gradle.kts
@@ -0,0 +1,66 @@
+import dependencies.implementDaggerHilt
+import dependencies.implementFirestore
+import dependencies.implementNavigation
+import dependencies.implementPaging3
+import dependencies.implementViewBindingDelegate
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    compileSdk = 30
+
+    defaultConfig {
+        minSdk = 23
+        targetSdk = 30
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
+
+    implementation(project(":core"))
+    implementation(project(":core-ui"))
+    implementation(project(":features:home"))
+    implementation(project(":features:profile"))
+    implementation(project(":features:favorites"))
+
+    implementPaging3()
+    implementFirestore()
+    implementDaggerHilt()
+    implementNavigation()
+    implementViewBindingDelegate()
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
