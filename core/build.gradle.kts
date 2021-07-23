61fae9244d49c88db0c955b537ca58e39d45832f
maximsmolyakov <sm.maksim@icloud.com>
2021-07-23 18:40:11 +0800

    Migrate core gradle files to Kotlin DSL

---
+++ core/build.gradle.kts
@@ -0,0 +1,49 @@
+import dependencies.implementDaggerHilt
+import dependencies.implementNavigation
+import dependencies.implementRoom
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-parcelize")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    compileSdk = 30
+
+    defaultConfig {
+        minSdk = 23
+        targetSdk = 30
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+
+    implementRoom()
+    implementDaggerHilt()
+    implementNavigation()
+}
