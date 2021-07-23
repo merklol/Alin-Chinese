d1ab5b8431c8f1411df72a55ba155b1dc316d73b
maximsmolyakov <sm.maksim@icloud.com>
2021-07-23 20:15:16 +0800

    Migrate authentication module gradle files to Kotlin DSL

---
+++ features/authentication/build.gradle.kts
@@ -0,0 +1,66 @@
+import dependencies.implementAuthentication
+import dependencies.implementCoroutines
+import dependencies.implementCoroutinesPlayServices
+import dependencies.implementDaggerHilt
+import dependencies.implementNavigation
+import dependencies.implementUniFlow
+import dependencies.implementViewBindingDelegate
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    compileSdk = 30
+
+    defaultConfig {
+        minSdk = 23
+        targetSdk = 30
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
+
+    implementUniFlow()
+    implementNavigation()
+    implementCoroutines()
+    implementDaggerHilt()
+    implementAuthentication()
+    implementViewBindingDelegate()
+    implementCoroutinesPlayServices()
+
+    implementation(project(":core-ui"))
+    implementation(project(":core"))
+
+    testImplementation("junit:junit:4.13.2")
+}
