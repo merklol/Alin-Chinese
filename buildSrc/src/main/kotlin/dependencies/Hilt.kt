4812efcf41a25d23cc1024433ec001857dc5e60a
maximsmolyakov <sm.maksim@icloud.com>
2021-07-23 22:25:24 +0800

    Add buildSrc

---
+++ buildSrc/src/main/kotlin/dependencies/Hilt.kt
@@ -0,0 +1,13 @@
+package dependencies
+
+import org.gradle.kotlin.dsl.DependencyHandlerScope
+
+fun DependencyHandlerScope.implementDaggerHilt() {
+    "implementation"("com.google.dagger:hilt-android:${Versions.hilt}")
+    "kapt"("com.google.dagger:hilt-compiler:${Versions.hilt}")
+
+    "androidTestImplementation"(
+        "com.google.dagger:hilt-android-testing:${Versions.hilt}"
+    )
+    "kaptAndroidTest"("com.google.dagger:hilt-compiler:${Versions.hilt}")
+}
---
+++ buildSrc/src/main/kotlin/dependencies/Versions.kt
@@ -0,0 +1,43 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Maxim Smolyakov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dependencies
+
+object Versions {
+    const val hilt = "2.37"
+    const val coil = "1.2.1"
+    const val room = "2.3.0"
+    const val gson = "2.8.6"
+    const val mockK = "1.12.0"
+    const val paging3 = "3.0.0"
+    const val uniFlow = "1.0.6"
+    const val navigation = "2.3.5"
+    const val firestore = "21.4.3"
+    const val lifecycle = "2.4.0-alpha01"
+    const val coroutines = "1.5.0-RC"
+    const val authentication = "21.0.1"
+    const val viewBindingDelegate = "1.4.6"
+    const val coroutinesPlayServices = "1.5.0"
+
+}
