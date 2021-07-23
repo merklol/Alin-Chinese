a8acc1aa645cf2932f9290d45170a04102240a72
maximsmolyakov <sm.maksim@icloud.com>
2021-07-23 21:03:36 +0800

    Migrate profile module gradle files to Kotlin DSL

---
+++ features/profile/build.gradle.kts
@@ -0,0 +1,56 @@
+import dependencies.implementDaggerHilt
+import dependencies.implementNavigation
+import dependencies.implementViewBindingDelegate
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    compileSdk = 30
+
+    defaultConfig {
+        minSdk = 23
+        targetSdk = 30
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    
+    implementNavigation()
+    implementDaggerHilt()
+    implementViewBindingDelegate()
+
+    implementation(project(":core-ui"))
+    implementation(project(":core"))
+}
