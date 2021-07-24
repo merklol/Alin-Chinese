fda6ac04538d6511070223b3b7b10cfdf08b75bf
maximsmolyakov <sm.maksim@icloud.com>
2021-07-24 13:21:34 +0800

    Update extensions in Project.kt

--- buildSrc/src/main/kotlin/extensions/Project.kt
+++ buildSrc/src/main/kotlin/extensions/Project.kt
@@ -1,8 +1,11 @@
 package extensions
 
+import com.android.build.api.dsl.JniLibsPackagingOptions
 import com.android.build.api.dsl.LibraryExtension
 import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
+import org.gradle.api.Action
 import org.gradle.api.Project
+import org.gradle.api.artifacts.ResolutionStrategy
 import org.gradle.kotlin.dsl.configure
 
 fun Project.configuration(applicationId: String, testInstrumentationRunner: String) =
@@ -10,6 +13,26 @@ fun Project.configuration(applicationId: String, testInstrumentationRunner: Stri
         defaultConfig {
             this.applicationId = applicationId
             this.testInstrumentationRunner = testInstrumentationRunner
+        }
+    }
+
+fun Project.configuration(
+    applicationId: String,
+    testInstrumentationRunner: String,
+    resolutionStrategy: Action<in ResolutionStrategy>,
+    jniLibs: JniLibsPackagingOptions.() -> Unit
+) =
+    configure<BaseAppModuleExtension> {
+        defaultConfig {
+            this.applicationId = applicationId
+            this.testInstrumentationRunner = testInstrumentationRunner
+            configurations.all {
+                this.resolutionStrategy(resolutionStrategy)
+            }
+        }
+
+        packagingOptions {
+            this.jniLibs(jniLibs)
         }
     }
 
