70e71e54371afbcc694fb916cdf0931100fb68a1
maximsmolyakov <sm.maksim@icloud.com>
2021-07-24 11:20:58 +0800

    Update favorites' build.gradle.kts

--- features/favorites/build.gradle.kts
+++ features/favorites/build.gradle.kts
@@ -4,52 +4,13 @@ import dependencies.implementUniFlow
 import dependencies.implementViewBindingDelegate
 
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
+    `android-feature-convention`
     id("kotlin-kapt")
     id("dagger.hilt.android.plugin")
     id("androidx.navigation.safeargs.kotlin")
 }
 
-android {
-    compileSdk = 30
-
-    defaultConfig {
-        minSdk = 23
-        targetSdk = 30
-
-        testInstrumentationRunner = "com.maximapps.favorites.HiltTestRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        viewBinding = true
-    }
-}
-
 dependencies {
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-
-
     implementDaggerHilt()
     implementViewBindingDelegate()
     implementUniFlow()
