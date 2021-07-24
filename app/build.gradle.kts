d193802051cdc4ab2923d70b6130f4006c016b7a
maximsmolyakov <sm.maksim@icloud.com>
2021-07-24 21:28:15 +0800

    Update app's build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,56 +4,22 @@ import dependencies.implementFirestore
 import dependencies.implementNavigation
 import dependencies.implementPaging3
 import dependencies.implementRoom
+import extensions.configuration
 
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
+    `android-app-convention`
     id("kotlin-kapt")
     id("dagger.hilt.android.plugin")
     id("com.google.gms.google-services")
     id("androidx.navigation.safeargs.kotlin")
 }
 
-android {
-    compileSdk = 30
-
-    defaultConfig {
-        applicationId = "com.maximapps.alinchinese"
-        minSdk = 23
-        targetSdk = 30
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        viewBinding = true
-    }
-}
+configuration(
+    applicationId = "com.maximapps.alinchinese",
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner",
+)
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-
     implementRoom()
     implementPaging3()
     implementFirestore()
