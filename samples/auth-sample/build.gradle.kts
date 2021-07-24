def3798e3e3b9dba532dcca2c9692f8d4f0536db
maximsmolyakov <sm.maksim@icloud.com>
2021-07-24 13:23:46 +0800

    Update auth sample app's build.gradle.kts

--- samples/auth-sample/build.gradle.kts
+++ samples/auth-sample/build.gradle.kts
@@ -2,65 +2,28 @@ import dependencies.implementAuthentication
 import dependencies.implementDaggerHilt
 import dependencies.implementMockk
 import dependencies.implementNavigation
+import extensions.configuration
 
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
+    `android-app-convention`
     id("kotlin-kapt")
     id("dagger.hilt.android.plugin")
     id("androidx.navigation.safeargs.kotlin")
     id("com.google.gms.google-services")
 }
 
-android {
-    compileSdk = 30
-
-    defaultConfig {
-        applicationId = "com.maximapps.authsample"
-        minSdk = 23
-        targetSdk = 30
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "com.maximapps.authsample.hilt.HiltTestRunner"
-
-        configurations.all {
-            resolutionStrategy {
-                force("androidx.test:monitor:1.4.0-rc01")
-            }
-        }
+configuration(
+    applicationId = "com.maximapps.authsample",
+    testInstrumentationRunner = "com.maximapps.authsample.hilt.HiltTestRunner",
+    resolutionStrategy = {
+        force("androidx.test:monitor:1.4.0-rc01")
+    },
+    jniLibs = {
+        useLegacyPackaging = true
     }
-    
-    packagingOptions {
-        jniLibs {
-            useLegacyPackaging = true
-        }
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
+)
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-
     implementAuthentication()
     implementDaggerHilt()
     implementNavigation()
@@ -68,7 +31,7 @@ dependencies {
     implementation(project(":core"))
     implementation(project(":core-ui"))
     implementation(project(":features:authentication"))
-    
+
     implementMockk()
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation(project(":test-utils"))
