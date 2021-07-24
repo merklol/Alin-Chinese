08def90d2f27b245b4bb46807da8ff43a5114d82
maximsmolyakov <sm.maksim@icloud.com>
2021-07-24 21:08:05 +0800

    Update home sample app's build.gradle.kts

--- samples/home-sample/build.gradle.kts
+++ samples/home-sample/build.gradle.kts
@@ -5,61 +5,27 @@ import dependencies.implementNavigation
 import dependencies.implementPaging3
 import dependencies.implementRoom
 import dependencies.implementRoomTesting
+import extensions.configuration
 
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
+    `android-app-convention`
     id("kotlin-kapt")
     id("dagger.hilt.android.plugin")
     id("com.google.gms.google-services")
 }
 
-android {
-    compileSdk = 30
-
-    defaultConfig {
-        applicationId = "com.maximapps.homesample"
-        minSdk = 23
-        targetSdk = 30
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "com.maximapps.homesample.hilt.HiltTestRunner"
-
-        configurations.all {
-            resolutionStrategy {
-                force("androidx.test:monitor:1.4.0-rc01")
-            }
-        }
+configuration(
+    applicationId = "com.maximapps.homesample",
+    testInstrumentationRunner = "com.maximapps.homesample.hilt.HiltTestRunner",
+    resolutionStrategy = {
+        force("androidx.test:monitor:1.4.0-rc01")
+    },
+    jniLibs = {
+        useLegacyPackaging = true
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        viewBinding = true
-    }
-}
+)
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-
     implementRoom()
     implementPaging3()
     implementFirestore()
