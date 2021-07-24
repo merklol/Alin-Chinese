38d060052b076c55a4ed8ff4601860e8c5e6a886
maximsmolyakov <sm.maksim@icloud.com>
2021-07-24 11:29:17 +0800

    Update authentication's build.gradle.kts

--- features/authentication/build.gradle.kts
+++ features/authentication/build.gradle.kts
@@ -6,50 +6,13 @@ import dependencies.implementUniFlow
 import dependencies.implementViewBindingDelegate
 
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
+    `android-feature-convention`
     id("kotlin-kapt")
     id("dagger.hilt.android.plugin")
     id("androidx.navigation.safeargs.kotlin")
 }
 
-android {
-    compileSdk = 30
-
-    defaultConfig {
-        minSdk = 23
-        targetSdk = 30
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        viewBinding = true
-    }
-}
-
 dependencies {
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.4.0")
-
     implementUniFlow()
     implementNavigation()
     implementCoroutines()
