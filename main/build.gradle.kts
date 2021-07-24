b6adecf58d89665a396297c91d8c8d2b279f3a96
maximsmolyakov <sm.maksim@icloud.com>
2021-07-24 11:35:01 +0800

    Update main's build.gradle.kts

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -5,49 +5,12 @@ import dependencies.implementPaging3
 import dependencies.implementViewBindingDelegate
 
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
+    `android-feature-convention`
     id("kotlin-kapt")
     id("dagger.hilt.android.plugin")
 }
 
-android {
-    compileSdk = 30
-
-    defaultConfig {
-        minSdk = 23
-        targetSdk = 30
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        viewBinding = true
-    }
-}
-
 dependencies {
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.4.0")
-
     implementation(project(":core"))
     implementation(project(":core-ui"))
     implementation(project(":features:home"))
