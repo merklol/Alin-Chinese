0abae4087cf320a47bae7f45e466941a5f74a824
maximsmolyakov <sm.maksim@icloud.com>
2021-07-24 12:12:58 +0800

    Update test-utils' build.gradle.kts

--- test-utils/build.gradle.kts
+++ test-utils/build.gradle.kts
@@ -1,35 +1,5 @@
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
-}
-
-android {
-    compileSdk = 30
-
-    defaultConfig {
-        minSdk = 23
-        targetSdk = 30
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    `test-module-convention`
 }
 
 dependencies {
