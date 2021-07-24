1214d8ac7337309c4e4a0fd91e0d863e41c7b6a2
maximsmolyakov <sm.maksim@icloud.com>
2021-07-24 13:22:47 +0800

    Add android-app convention plugin

---
+++ buildSrc/src/main/kotlin/android-app-convention.gradle.kts
@@ -0,0 +1,38 @@
+import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-convention")
+}
+
+configure<BaseAppModuleExtension> {
+    compileSdk = 30
+
+    defaultConfig {
+        minSdk = 23
+        targetSdk = 30
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    dependencies {
+        implementation("androidx.core:core-ktx:1.6.0")
+        implementation("androidx.appcompat:appcompat:1.3.1")
+        implementation("com.google.android.material:material:1.4.0")
+    }
+}
