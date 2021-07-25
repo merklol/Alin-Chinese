395681ce230e0cf11a2f3ec6d7ffa525ea6b6eec
maximsmolyakov <sm.maksim@icloud.com>
2021-07-26 01:09:46 +0800

    Migration from Kotlin 1.5.20 to Kotlin 1.5.30-M1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,12 @@
 buildscript {
-
     repositories {
         google()
         mavenCentral()
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.0-alpha04")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
+        classpath("com.android.tools.build:gradle:7.1.0-alpha03")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30-M1")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.37")
         classpath("com.google.gms:google-services:4.3.8")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
