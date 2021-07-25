555c57aef5c85dfe3ee6f76146dfe10b2b7a1645
maximsmolyakov <sm.maksim@icloud.com>
2021-07-25 12:39:33 +0800

    Update android-library-convention.gradle.kts

--- buildSrc/src/main/kotlin/android-library-convention.gradle.kts
+++ buildSrc/src/main/kotlin/android-library-convention.gradle.kts
@@ -1,5 +1,35 @@
-import com.android.build.gradle.LibraryExtension
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Maxim Smolyakov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
+import com.android.build.gradle.LibraryExtension
+import gradle.kotlin.dsl.accessors._7c271eebf31aea983623dfec31c230a6.implementation
+
+/**
+ * Gradle convention plugin for Android library modules.
+ *
+ * @since 0.5.2
+ */
 plugins {
     id("com.android.library")
     id("kotlin-android")
@@ -35,8 +65,8 @@ configure<LibraryExtension> {
     }
 
     dependencies {
-        implementation("androidx.core:core-ktx:1.6.0")
-        implementation("androidx.appcompat:appcompat:1.3.1")
-        implementation("com.google.android.material:material:1.4.0")
+        implementation(StandardLibraries.CoreKtx)
+        implementation(StandardLibraries.AppCompat)
+        implementation(StandardLibraries.Material)
     }
 }
