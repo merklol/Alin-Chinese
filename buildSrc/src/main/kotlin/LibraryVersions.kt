f26370d38d0699d2f2b6de42c77cdc7c0ca3c99b
maximsmolyakov <sm.maksim@icloud.com>
2021-07-25 12:32:31 +0800

    Add LibraryVersions.kt

---
+++ buildSrc/src/main/kotlin/LibraryVersions.kt
@@ -0,0 +1,53 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Maxim Smolyakov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * Current libraries' versions.
+ *
+ * @since 0.5.2
+ */
+object LibraryVersions {
+    const val Hilt = "2.37"
+    const val Coil = "1.2.1"
+    const val Room = "2.3.0"
+    const val Gson = "2.8.6"
+    const val JUnit = "4.13.2"
+    const val MockK = "1.12.0"
+    const val CoreKtx = "1.6.0"
+    const val Paging3 = "3.0.0"
+    const val UniFlow = "1.0.6"
+    const val Espresso = "3.4.0"
+    const val Material = "1.4.0"
+    const val AppCompat = "1.3.1"
+    const val Firestore = "21.4.3"
+    const val Navigation = "2.3.5"
+    const val Lifecycle = "2.4.0-alpha01"
+    const val Coroutines = "1.5.0-RC"
+    const val AndroidXJunit = "1.1.3"
+    const val Authentication = "21.0.1"
+    const val EspressoContrib = "3.4.0"
+    const val FragmentTesting = "1.3.6"
+    const val NavigationTesting = "2.3.5"
+    const val ViewBindingDelegate = "1.4.6"
+}
